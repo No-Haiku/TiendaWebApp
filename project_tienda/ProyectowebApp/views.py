a4bcac6b07bd4a29f20d351c23ef5ec985fdc249
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-01-12 20:14:54 -0300

    servicios se muestra en fronted
    se agrego la vista de servicios a la pagina web

--- project_tienda/ProyectowebApp/views.py
+++ project_tienda/ProyectowebApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from servicios.models import Servicio
 
 # Create your views here.
 
@@ -7,7 +8,9 @@ def home(request):
     return render(request,"ProyectowebApp/home.html")
 def servicios(request):
 
-    return render(request,"ProyectowebApp/servicios.html")
+    servicios=Servicio.objects.all()#importo todos los servicios del objeto servicio
+
+    return render(request,"ProyectowebApp/servicios.html",{"servicios":servicios})
 
 def tienda(request):
 
